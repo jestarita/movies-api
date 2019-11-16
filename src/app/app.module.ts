95b846c83c31226129464c4d60589dd14b449386
Jorge Estarita <jestaritacardozo@gmail.com>
2019-11-15 22:02:36 -0500

    segundo commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {NgxPaginationModule} from 'ngx-pagination';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
+import { FilterPipe } from './Pipes/filter/filter.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    NgbModule
+    NgbModule,
+    FormsModule,
+    NgxPaginationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input  } from '@angular/core';
-import {ApiService} from '../../Services/api.service';
+import {ApiService} from '../../Services/api/api.service';
 import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -8,10 +9,13 @@ import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
   providers: [NgbModalConfig, NgbModal]
 })
 export class HomeComponent implements OnInit {
-  @Input() name;
+
+
 public info:any;
 public params:any;
-
+private genres:any;
+public lista:[];
+itemsPerPage = 6;
   constructor(private api:ApiService, config: NgbModalConfig,
      private modalService: NgbModal) { 
       config.backdrop = 'static';
@@ -20,6 +24,7 @@ public params:any;
 
   ngOnInit() {
     this.get_lista();
+    this.Get_genres();
   }
 
   get_lista(){
@@ -32,11 +37,47 @@ public params:any;
       }
     )
   }
-  open(content, movie) {
-    const modalRef = this.modalService.open(content);
-    this.params= movie;
+  
+
+
+  Get_genres(){
+    var listado:any = [];
+    this.api.getmovies().subscribe(
+      (data: any)=>{
+        this.genres= data.results;
+        this.genres.forEach(element => {
+
+         
+         console.log(element.generos);
+        
+
+
+        });
+      }, 
+      error =>{
+        console.log(<any>error);
+      }
+    )
+
   }
 
+  // delete_duplicates(generos){
+  //   var newArray = [];
+  //   var lookupObject = {};
+  //   for(var i in generos) {
+  //   lookupObject[i] = generos[i];
+  //   }
+  //   for(i in lookupObject) {
+  //   newArray.push(lookupObject[i]);
+  //   }
 
+  //  // console.log(newArray)
+  //   return newArray;
+  //   }
 
-}
+    open(content, movie) {
+      const modalRef = this.modalService.open(content);
+      this.params= movie;
+    }
+  
+  }
