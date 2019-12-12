4f982d967b18f09d322605d7e4aa38108de8e91d
frank-carpio <frankcarpio8@gmail.com>
2019-12-11 20:01:01 -0500

    fix modaldetail

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -7,65 +7,48 @@ import { faStar } from '@fortawesome/free-solid-svg-icons';
 })
 export class DetailsComponent implements OnInit {
 
-  @Input() movie: any;  
+  @Input() movie: any;
   faStar = faStar;
   @Output() closeModalEvent = new EventEmitter<boolean>();
-  @Input() modalVisible:any;
-  constructor() {   
-    
-   }
+  @Input() modalVisible: any;
+  constructor() {}
 
-  ngOnInit() {    
- 
+  ngOnInit() {
   this.exists1('nombre');
   this.exists2('nombre');
- 
-  } 
+  }
 
-  onCloseModal(event: any){
-     
-    this.movie = null;
+  onCloseModal(event: any) {
+    this.closeModalEvent.emit(false);
    }
 
 
 
   exists1(parametros) {
-     
-    if (localStorage.getItem(parametros)) {
-      return false;
-    } else {
-      return true;
-    }
+    return !localStorage.getItem(parametros);
   }
 
   exists2(parametros) {
-    if (localStorage.getItem(parametros)) {
-      return true;
-    } else {
-      return false;
-    }
+    return !!localStorage.getItem(parametros);
   }
 
-    //method to add movies to favorites
+    // method to add movies to favorites
     add_favorites(params) {
       if (localStorage.getItem(params)) {
-      console.log('la pelicula ya esta agregada'); 
+      console.log('la pelicula ya esta agregada');
       } else {
         localStorage.setItem(params, params);
-      }  
-    }
-  
-  
-    //method to remove movies from favorites
-    remove_favorites(params) {
-      if (localStorage.getItem(params)) {
-        localStorage.removeItem(params);     
-      } else {
-       console.log('la pelicula no existe')
       }
     }
 
-  
 
+    // method to remove movies from favorites
+    remove_favorites(params) {
+      if (localStorage.getItem(params)) {
+        localStorage.removeItem(params);
+      } else {
+       console.log('la pelicula no existe');
+      }
+    }
 
 }
